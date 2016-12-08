fc3fc1935669ea4f4dfefc01e4d44b49ca02283c
wangchun <1013812915@qq.com>
2016-12-08 23:01:02 +0800

    添加工程文件

--- 我的测试程序/stringTest/stringTest.cpp
+++ 我的测试程序/stringTest/stringTest.cpp
@@ -9,8 +9,8 @@ int main()
 	char a[3] = {'e'};
 	std::cout << strlen(a) << std::endl;  //计算到NULL之前， 所以是不确定的长度
 
-	char src[] = "abcdefg";
-	char *dst = "abc";
+	char src[] = "ag";
+	char dst[] = "abc";
 	strcpy(dst, src);            //运行错误
 	return 0;
 }
